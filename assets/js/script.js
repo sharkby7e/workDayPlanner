2a29546360cf91f2465b8ee460e069db7894cfeb
sharkbyte <sgquin@gmail.com>
2022-04-06 23:35:54 -0500

    built one row using jquery methods and bootstrap classes, centered it as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@ var container = $('.container')
 
 current.text(today.format("[Today is ]dddd, MMMM Do"))
 
+
+var newRow = $('<div>').addClass('row')
+
+
+var newHour = $("<div>").addClass('hour col')
+var newText = $("<div>").addClass('present col-10 ')
+var newBtn = $("<div>").addClass('saveBtn col ')
+container.append(newRow)
+newHour.text("9am")
+newRow.append(newHour)
+newRow.append(newText)
+newRow.append(newBtn)
+
